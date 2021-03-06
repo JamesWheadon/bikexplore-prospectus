37abb8f89ba48ee8d37cbeeba092f523084426b0
James Wheadon <james.wheadon@yahoo.com>
2021-03-06 16:49:35 +0000

    add inputs for forms and prospectus

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function addOverviewImage() {
     let numOverviewImages = document.getElementsByClassName('overviewImages').length
-    let imageInputLabel = document.createElement('label')
+    let imageInputLabel = `<label for="overviewImage${numOverviewImages + 1}">URL for image for overview tab:</label>`
     let imageInput = `<input name="overviewImage${numOverviewImages + 1}" id="overviewImage${numOverviewImages + 1}" class="OverviewImages" placeholder="URL of an image for the overview tab"/>`
+    document.getElementById('overviewImagesDiv').innerHTML += imageInputLabel;
     document.getElementById('overviewImagesDiv').innerHTML += imageInput;
+    document.getElementById('overviewImagesDiv').innerHTML += "<br>";
 }
